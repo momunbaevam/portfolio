77faba9003b0169849272eb76e3a578b7e055023
momunbaevam <93367394+momunbaevam@users.noreply.github.com>
2021-11-19 15:00:27 +0600

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Meerim's portfolio</title>
+</head>
+
+<style>
+    h1 {
+        font-family: verdana;
+    }
+    
+    h3 {
+        color: rgb(255, 0, 149);
+        font-family: serif;
+    }
+    
+    p {
+        font-family: Helvetica;
+    }
+    
+    .button {
+        border: none;
+        color: rgb(255, 255, 255);
+        padding: 32px 64px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 7px 3px;
+        transition-duration: 1.9s;
+        cursor: pointer;
+    }
+    
+    .button1 {
+        background-color: rgb(255, 255, 255);
+        color: black;
+        border: 2px solid black;
+    }
+    /* Github */
+    
+    .button1:hover {
+        background-color: black;
+        color: white;
+    }
+    
+    .button2 {
+        background-color: white;
+        color: black;
+        border: 2px solid rgb(255, 20, 20);
+    }
+    /* Instagram */
+    
+    .button2:hover {
+        background-color: rgb(255, 20, 20);
+        color: rgb(3, 3, 3);
+    }
+    .button3 {
+        background-color: rgb(255, 255, 255);
+        color: rgb(255, 0, 0);
+        border: 2px solid rgb(30, 173, 255);
+    }
+    /* Telegram */
+    
+    .button3:hover {
+        background-color: rgb(30, 165, 255);
+        color: black;
+    }
+    
+</style>
+<center>
+
+    <body style="background-color:rgb(245, 220, 242);">
+    <br>
+   <br>
+   <br>
+        <h1>Meerim Momunbaeva <br>Smm specialist </h1>
+        <h3>Can change your vision about social marketing!</h3>
+        <p> Start your business on the internet with me</p>
+        <img src="SMM.png"  style='width:200px;height:200px;  margin-left: 900px;'> 
+		<form action='https://github.com/momunbaevam/site/blob/main/style.css' target='_blank'>
+        <button class="button button1">Github</button>
+        </form>
+        <form action='https://www.instagram.com/momunbaevam/' target='_blank'>
+        <button class="button button2">Instagram</button>
+        </form>
+        <form action='https://web.telegram.org/z/' target='_blank'>
+        <button class='button button3'>Telegram</button>
+        </form>
+        <h4>My address: Ankara 1/8 <br>My phone number:+996703848891</h4>
+    </body>
+</center>
+</html>
